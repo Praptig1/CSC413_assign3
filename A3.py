f66d4b3785a88799e0646eff88f6f4e3a608b5f1
Prapti G <125534176+Praptig1@users.noreply.github.com>
2024-12-02 16:05:55 -0800

    Update A3.py

--- A3.py
+++ A3.py
@@ -10,6 +10,8 @@ baud_rate = 9600
 image_folder = r"C:\Users\af34j\OneDrive\Desktop\School\CSC413\A3\IRLED\images\today"  # Replace with the folder containing your images
 accepted_folder = r"C:\Users\af34j\OneDrive\Desktop\School\CSC413\A3\IRLED\images\all"  # Folder to save all accepted images
 favorited_folder = r"C:\Users\af34j\OneDrive\Desktop\School\CSC413\A3\IRLED\images\favorites"  # Folder to save favorited images
+gallery = r"C:\Users\af34j\OneDrive\Desktop\School\CSC413\A3\IRLED\images\gallery.png"  # Folder to save favorited images
+
 
 # Ensure folders exist
 os.makedirs(accepted_folder, exist_ok=True)
@@ -47,6 +49,19 @@ def show_image(index):
     image_label.configure(image=tk_image)
     image_label.image = tk_image
 
+def show_gallery():
+    global image_label
+    img = Image.open(gallery)
+
+    # *** Resize to fit the fullscreen ***
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    img = img.resize((screen_width, screen_height), Image.Resampling.LANCZOS)
+
+    tk_image = ImageTk.PhotoImage(img)
+    image_label.configure(image=tk_image)
+    image_label.image = tk_image
+
 # Serial communication setup
 ser = serial.Serial(serial_port, baud_rate)
 
@@ -68,7 +83,10 @@ def handle_command(command):
         print(f"Marked for deletion: {images[current_image_index]}")
     elif command == "ENTER":
         save_images()
-        print("Images saved! Exiting.")
+        show_gallery()
+        return
+    elif command == "POWER":
+        print("Exiting")
         root.quit()
 
     # Show updated image
@@ -80,28 +98,4 @@ def save_images():
     for image_path in images:
         if image_path in deleted:
             continue  # Skip deleted images
-        elif image_path in favorites:
-            save_image_to_folder(image_path, favorited_folder)
-        else:
-            save_image_to_folder(image_path, accepted_folder)
-
-# Function to save a single image to a folder
-def save_image_to_folder(image_path, folder):
-    filename = os.path.basename(image_path)
-    destination = os.path.join(folder, filename)
-    shutil.copy(image_path, destination)
-
-# Continuously listen for commands
-def listen_to_arduino():
-    if ser.in_waiting > 0:
-        command = ser.readline().decode('utf-8').strip()
-        handle_command(command)
-    root.after(100, listen_to_arduino)
-
-# Start the application
-if images:
-    show_image(current_image_index)
-    root.after(100, listen_to_arduino)  # Start listening to Arduino
-    root.mainloop()
-else:
-    print("No images found in the folder!")
+... (26 lines left)
